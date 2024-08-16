14065d6bafd4d3b4887de53ae0bbde236ccb6d13
marianaduda <marianaeduarda197@gmail.com>
2024-08-16 12:02:18 -0300

    NOME, IDADE E ALTURA
    nome = "Mariana Eduarda"
    idade= 16
    altura = 1,82
    
    print("olá", nome, "a sua idade é", idade)
    
    idade = 16
    Nome = "Mel"
    altura = 1,57
    
    print("olá", nome,"a sua idade é",idade)

---
+++ aula01.py
@@ -0,0 +1,11 @@
+nome = "Mariana Eduarda"
+idade= 16
+altura = 1,82
+
+print("olá", nome, "a sua idade é", idade)
+
+idade = 16
+Nome = "Mel"
+altura = 1,57
+
+print("olá", nome,"a sua idade é",idade)
