3c8a971064233aa52adf9154ee22745705cdd241
marianaduda <marianaeduarda197@gmail.com>
2024-08-20 11:43:17 -0300

    Media de Notas

---
+++ Media.py
@@ -0,0 +1,8 @@
+#Algoritmo de média: criar um algoritmo que leia 4 notas e  apresente a media ao final
+
+nota1 = float(input("Digite a primeira nota:" ))
+nota2 =float (input("Digite a segunda nota:" ))
+nota3 =float (input("Digite a terceira nota:" ))
+nota4 =float (input("Digite a quarta nota:" ))
+media = (nota1 + nota2 + nota3 + nota4 ) / 4
+print("A média final obtida ", media)
