bb6e127efb96a83151a7f66962beda15abb4b43f
marianaduda <marianaeduarda197@gmail.com>
2024-08-20 11:42:19 -0300

    Aula 02

---
+++ aula02.py
@@ -0,0 +1,38 @@
+# comentários de uma linha 
+# Tipágem dinâmica
+
+# teste = "olá" 
+# print(type(teste))
+# teste = 30
+# print(type(teste))
+# teste = 2.4
+# print(type(teste))
+
+# nome = input("Digite o seu nome: ") 
+# print("olá" , nome)
+# idade = input("Digite a sua idade: ")
+# print(idade)
+# print (type(idade))
+
+#Tipos de Dados:
+#int: números inteiros
+#float: Números reais, decimais
+#str: string, cadeia de caracteres, utilizando aspas
+#boolean: tipo lógico true ou falso
+
+# valorA = int(input("Digite o valor de A: "))
+# valorB = inte(input("Digite o valor de B: "))
+# print(valorA + valorB)
+# print(type(valorA))
+
+#operadores artiméticos:
+x = 3
+print(x + 1) #Adição
+print(x - 1) #Subtração
+print(x * 2) #Multiplicação
+print(x / 2) #Divisão
+print(x // 2) #Divisão inteira
+print(x ** 2) #Exponenciação
+print(x % 2) #Resto da divisãõ / Módulo
+
+
