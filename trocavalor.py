3941c31fad0cf68ef58feba5822441bd7048382a
marianaduda <marianaeduarda197@gmail.com>
2024-08-23 11:49:58 -0300

    Troca De Valores

---
+++ trocavalor.py
@@ -0,0 +1,15 @@
+#Crie um algoritmo que armazene dois valores distintos (A= 5 e B= 10) e efetue a troca dos valores de forma que as variável
+# A passe a possuir o valor da variável B e que a variáve B passe a possuir o valor de A. Por fim, apresentar os valores trocados.
+# (A=10 e B=5)
+
+
+a= 5
+b= 10
+print("Os valores de A e B são respectivamente", a, "e", b)
+auxiliar = a
+a = b
+b = auxiliar
+print("Agora A=", a,"e B=", b)
+
+
+
