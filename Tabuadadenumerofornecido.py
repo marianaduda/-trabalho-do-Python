c310290a8a228ab47a1d3e4cc928c2e8d875a8de
marianaduda <marianaeduarda197@gmail.com>
2024-11-04 11:26:07 -0300

    Tabuada do For

---
+++ Tabuadadenumerofornecido.py
@@ -0,0 +1,7 @@
+#Ylena Maria 
+# Loop para cada número da tabuada
+for i in range(1, 11):  # de 1 a 10
+    print(f'Tabuada do {i}:')
+    for j in range(1, 11):  # de 1 a 10
+        print(f'{i} x {j} = {i * j}')
+    print()  # Linha em branco entre as tabuadas
